6d569c22c71b880a68e94a84c624ecd920df75bc
hellckt <scottzkt@gmail.com>
2014-10-06 15:27:14 +0800

    new file:   .gitignore 	new file:   app/__init__.py 	new file:   app/api_1_0/__init__.py 	new file:   app/api_1_0/authentication.py 	new file:   app/api_1_0/comments.py 	new file:   app/api_1_0/decorators.py 	new file:   app/api_1_0/errors.py 	new file:   app/api_1_0/posts.py 	new file:   app/api_1_0/user.py 	new file:   app/api_1_0/users.py 	new file:   app/auth/__init__.py 	new file:   app/auth/forms.py 	new file:   app/auth/templates/auth/email/confirm.txt 	new file:   app/auth/views.py 	new file:   app/decorators.py 	new file:   app/email.py 	new file:   app/exceptions.py 	new file:   app/main/__init__.py 	new file:   app/main/errors.py 	new file:   app/main/forms.py 	new file:   app/main/views.py 	new file:   app/models.py 	new file:   app/models.py~ 	new file:   app/static/favicon.ico 	new file:   app/static/styles.css 	new file:   app/templates/403.html 	new file:   app/templates/404.html 	new file:   app/templates/500.html 	new file:   app/templates/_comments.html 	new file:   app/templates/_macros.html 	new file:   app/templates/_posts.html 	new file:   app/templates/auth/change_email.html 	new file:   app/templates/auth/change_password.html 	new file:   app/templates/auth/email/change_email.html 	new file:   app/templates/auth/email/change_email.txt 	new file:   app/templates/auth/email/confirm.html 	new file:   app/templates/auth/email/confirm.txt 	new file:   app/templates/auth/email/reset_password.html 	new file:   app/templates/auth/email/reset_password.txt 	new file:   app/templates/auth/login.html 	new file:   app/templates/auth/register.html 	new file:   app/templates/auth/reset_password.html 	new file:   app/templates/auth/unconfirmed.html 	new file:   app/templates/base.html 	new file:   app/templates/edit_post.html 	new file:   app/templates/edit_profile.html 	new file:   app/templates/error_page.html 	new file:   app/templates/followers.html 	new file:   app/templates/index.html 	new file:   app/templates/mail/new_user.html 	new file:   app/templates/mail/new_user.txt 	new file:   app/templates/moderate.html 	new file:   app/templates/post.html 	new file:   app/templates/user.html 	new file:   config.py 	new file:   manage.py 	new file:   migrations/README 	new file:   migrations/alembic.ini 	new file:   migrations/env.py 	new file:   migrations/script.py.mako 	new file:   migrations/versions/2d8bf4568eb4_.py 	new file:   requirements/common.txt 	new file:   requirements/dev.txt 	new file:   requirements/heroku.txt 	new file:   requirements/prod.txt 	new file:   tests/__init__.py 	new file:   tests/test_api.py 	new file:   tests/test_basics.py 	new file:   tests/test_client.py 	new file:   tests/test_selenium.py 	new file:   tests/test_user_model.py

---
+++ manage.py
@@ -0,0 +1,83 @@
+# -*- coding=utf-8 -*-
+#!/usr/bin/env python
+
+import os
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+if os.path.exists('.env'):
+    print('从 .env 中导入环境变量...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
+from app import create_app, db
+from app.models import User, Follow, Role, Permission, Post, Comment
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post, Comment=Comment)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test(coverage=False):
+    """运行单元测试。"""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print(u'Coverage 汇总:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print(u'HTML 版本: file://%s/index.html' % covdir)
+        COV.erase()
+
+
+@manager.command
+def profile(length=25, profile_dir=None):
+    """在 code profiler 下运行应用。"""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+
+@manager.command
+def deploy():
+    """执行拓展任务。"""
+    from flask.ext.migrate import upgrade
+    from app.models import Role, User
+
+    # migrate database to latest revision
+    upgrade()
+
+    # create user roles
+    Role.insert_roles()
+
+    # create self-follows for all users
+    User.add_self_follows()
+
+
+if __name__ == '__main__':
+    manager.run()
